6ecae5c9cd2bb2fee612a23126036c3f1608550e
Dillon Baxendell <dillon.j.baxendell@gmail.com>
2021-04-27 17:22:08 -0500

    problems 1-2 complete

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -25,7 +25,9 @@ console.log('A little about me:', me);
   - Console.log fullName
 */
 
+let fullName = me.firstName + ' ' + me.lastName;
 
+console.log( fullName );
 
 /* 3. Nested arrays
   - Console.log your first favorite food
